6c762f2298ef3e842abfa01d2ff8f52def7edb21
Eric Hu <ehu987@gmail.com>
2020-04-08 11:45:53 -0400

    added animate2 and multi animate

--- visualization.py
+++ visualization.py
@@ -1,7 +1,7 @@
 import numpy as np
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
-def animate(x, y, title):
+def animate(x, y, title, xlim, ylim):
     ### animate takes in N x 2 numpy arrays of data. x tracks the path of the predator (red), and y tracks the path of the prey (blue) ###
     n = np.size(x, 0)
 
@@ -14,8 +14,8 @@ def animate(x, y, title):
               ],
         # config axes
         layout=go.Layout(
-            xaxis=dict(range=[-10, 10],autorange=False, zeroline=False),
-            yaxis=dict(range=[-10, 10],autorange=False, zeroline=False),
+            xaxis=dict(range=[-xlim, xlim],autorange=False, zeroline=False),
+            yaxis=dict(range=[-ylim, ylim],autorange=False, zeroline=False),
             title=title,
             updatemenus=[dict(
                 type="buttons",
@@ -51,6 +51,58 @@ def animate(x, y, title):
 
     fig.show()
 
+def multi_animate(x, y, title):
+    ### animate takes in N x 2 numpy arrays of data. x tracks the path of the predator (red), and y tracks the path of the prey (blue) ###
+    n = len(x)
+    sizes = [len(x[k]) for k in range(10)]
+    x_all = np.vstack(x)
+    y_all = np.vstack(y)
+    xlim = np.max(x_all) + 5
+    ylim = np.max(y_all) + 5
+    fig = go.Figure(
+        # initial data
+        data=[go.Scatter(x=x_all[:, 0], y=x_all[:, 1], marker=dict(color="rgba(255, 0, 0, 0.5)")),
+              go.Scatter(x=y_all[:, 0], y=y_all[:, 1], marker=dict(color="rgba(0,0,255, 0.5)")),
+              go.Scatter(x=x_all[:, 0], y=x_all[:, 1], marker=dict(color="rgba(255, 0, 0, 0.5)")),
+              go.Scatter(x=y_all[:, 0], y=y_all[:, 1], marker=dict(color="rgba(0,0,255, 0.5)"))
+              ],
+        # config axes
+        layout=go.Layout(
+            xaxis=dict(range=[-xlim, xlim], autorange=False, zeroline=False),
+            yaxis=dict(range=[-ylim, ylim], autorange=False, zeroline=False),
+            title=title,
+            updatemenus=[dict(
+                type="buttons",
+                buttons=[dict(label="Play", method="animate", args=[None])]
+            )]
+        ),
+        # config frames
+        frames=[go.Frame(
+            data=[
+                go.Scatter( # predator data
+                    x=[x[j][k, 0]], y=[x[j][k, 1]],
+                    mode="markers", marker_symbol=202,
+                    marker=dict(color="red", size=10)
+                ),
+                go.Scatter(
+                    x=[y[j][k, 0]], y=[y[j][k, 1]], # prey data
+                    mode="markers", marker_symbol=200,
+                    marker=dict(color="blue", size=10)
+                ),
+                go.Scatter(
+                    x=x[j][:, 0], y=x[j][:, 1],  # predator trace
+                    mode="lines",
+                    line=dict(color="rgba(255,0,0,0.5)", width=1)
+                ),
+                go.Scatter(
+                    x=y[j][:, 0], y=y[j][:, 1],  # prey trace
+                    mode="lines",
+                    line=dict(color="rgba(0,0,255,0.5)", width=1)
+                )
+            ], traces=[0, 1, 2, 3]
+        ) for j in range(n) for k in range(sizes[j])]
+    )
+    fig.show()
 
 def make_layout():
     return [dict(
@@ -74,12 +126,10 @@ def make_frames(data1, data2):
               go.Scatter(x=data2[:, 0], y=data2[:, 1],
                          mode="lines",
                          line=dict(color="rgba(0,0,255,0.5)", width=1)),
-              go.Heatmap(z=[[1, 2],
-                            [2, 1]],
-                         x=['fast action', 'slow action'],
-                         y=['unaware state', 'aware state'])
+              go.Image(z=[[10, 2],
+                            [2, 1]]),
               ],
-        traces=[0,1,2,3,4]
+        traces=[0, 1, 2, 3, 4]
     ) for k in range(n)]
 
 
@@ -89,12 +139,10 @@ def animate2(data1, data2):
     fig.add_scatter(x=data2[:, 0], y=data2[:, 1], mode="lines", line=dict(color="blue"), row=1, col=1)
     fig.add_scatter(x=data1[:, 0], y=data1[:, 1], mode="lines", line=dict(color="red"), row=1, col=1)
     fig.add_scatter(x=data2[:, 0], y=data2[:, 1], mode="lines", line=dict(color="blue"), row=1, col=1)
-    fig.add_heatmap(z=[[1,2],
+    fig.add_image(z=[[1,2],
                        [1,2]],
-                    x=['fast action', 'slow action'],
-                    y=['unaware state', 'aware state'],
-                    row=1, col=2
-                    )
+                  row=1,
+                  col=2)
     fig.update(frames=make_frames(data1, data2))
     fig.update_layout(updatemenus=make_layout())
     fig.update_xaxes(range=[-5, 5], autorange=False, row=1, col=1)
